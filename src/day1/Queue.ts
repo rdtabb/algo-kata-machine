2d9295a675714578f8011e9111c35ed5c884c2f3
Rinat Tabaev <rdtabaev@edu.hse.ru>
2023-04-24 17:04:01 +0300

    I've done Linear and Binary searches, Two Crystal Balls problem and the Buuble sort. Now I am implementing my first Queue

---
+++ src/day1/Queue.ts
@@ -0,0 +1,33 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>
+}
+
+export default class Queue<T> {
+    public length: number;
+    private head?: Node<T>;
+    private tail?: Node<T>;
+
+    constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
+    }
+
+    enqueue(item: T): void {
+    }
+    deque(): T | undefined {
+        if (!this.head) {
+            return undefined
+        }
+
+        this.length--;
+        const head = this.head
+        this.head = this.head.next
+        head.next = undefined
+
+        return head.value
+    }
+    peek(): T | undefined {
+        return this.head?.value;
+    }
+}
---
+++ src/day1/SinglyLinkedList.ts
@@ -0,0 +1,25 @@
+export default class SinglyLinkedList<T> {
+    public length: number;
+
+    constructor() {
+    }
+
+    prepend(item: T): void {
+
+    }
+    insertAt(item: T, idx: number): void {
+
+    }
+    append(item: T): void {
+        return undefined
+    }
+    remove(item: T): T | undefined {
+        return undefined
+    }
+    get(idx: number): T | undefined {
+        return undefined 
+    }
+    removeAt(idx: number): T | undefined {
+        return undefined
+    }
+}
